e0ea8eba20ef8c4367d242a66faa65da090e7e1c
KemalIlkilic <kemalonur.ilkilic@bahcesehir.edu.tr>
2023-10-22 18:06:08 +0300

    project finish

--- src/App.js
+++ src/App.js
@@ -25,33 +25,54 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
-      {data.map((item, i) => (
+      {data.map((el, i) => (
         <AccordionItem
-          title={item.title}
-          text={item.text}
-          num={i + 1}
-          key={i}
-        />
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
       ))}
+
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title="Test 1"
+        num={22}
+        key="test 1"
+      >
+        <p>Allows React developers to:</p>
+        <ul>
+          <li>Break up UI into components</li>
+          <li>Make components reusuable</li>
+          <li>Place state efficiently</li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
 
   function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
+    onOpen(isOpen ? null : num);
   }
 
   return (
-    <div className={isOpen ? "item open" : "item"} onClick={handleToggle}>
-      <p className="number">{num}</p>
-      <p className="title">{title}</p>dio
-      <p className="icon">{!isOpen ? "+" : "-"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
